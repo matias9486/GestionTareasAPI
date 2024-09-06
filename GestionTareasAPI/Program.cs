4f70e72e31577080e4009d2a30c19ed48f31c1da
matias9486 <matias9486mdp@gmail.com>
2024-09-06 16:18:57 -0300

    feat: agregado capa de presentacion. Con controllers, seedData.

--- GestionTareasAPI/Program.cs
+++ GestionTareasAPI/Program.cs
@@ -1,6 +1,23 @@
+using GestionTareas_BusinessLayer.Exceptions;
+using GestionTareas_BusinessLayer.Interfaces;
+using GestionTareas_BusinessLayer.Services;
+using GestionTareas_DataAccessLayer.Data;
+using GestionTareas_DataAccessLayer.Interfaces;
+using GestionTareas_DataAccessLayer.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//agregar context y configuracion para conectarse a sql server
+builder.Services.AddDbContext<GestionTareasDbContext>(
+    options => options.UseSqlServer(
+        builder.Configuration.GetConnectionString("ConexionSql"))
+);
+
+//Declarar servicios
+builder.Services.AddScoped<ITareasRepository, TareasRepository>();
+builder.Services.AddScoped<ITareasService, TareasServices>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +25,22 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+//Using para recursos
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<GestionTareasDbContext>();
+
+    //context.Database.EnsureCreated();
+    context.Database.Migrate(); //aplica las migraciones pendientes
+    //Cargar datos iniciales
+    SeedData.Initialize(context);
+}
+
+// Configurar el middleware para manejar excepciones.
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ GestionTareasAPI/SeedData.cs
@@ -0,0 +1,82 @@
+﻿using GestionTareas_DataAccessLayer.Data;
+using GestionTareas_DataAccessLayer.Models;
+
+public class SeedData
+{
+    public static void Initialize(GestionTareasDbContext context) {
+        if (!context.Usuario.Any())
+        {
+
+            // Agregar datos de usuarios
+            var usuarios = new[]
+            {
+                new Usuario {
+                    Name = "Matias",
+                    Username = "Argento",
+                    Email = "matias@gmail.com",
+                    Password = "1234"
+                },
+                new Usuario {
+                    Name = "Fernando",
+                    Username = "Nando",
+                    Email = "nando@gmail.com",
+                    Password = "1234",
+                }
+            };
+            context.Usuario.AddRange(usuarios);
+            context.SaveChanges();
+        }
+
+        if (!context.Tarea.Any())
+        {
+            // Agregar datos de tareas
+            var tareas = new[]
+            {
+                new Tarea {
+                    Descripcion = "Crear entidades",
+                    FechaCreacion = DateTime.Now,
+                    IsTerminada = false,
+                    UsuarioId = 1
+                },
+                new Tarea {
+                    Descripcion = "Crear context",
+                    FechaCreacion = DateTime.Now,
+                    IsTerminada = false,
+                    UsuarioId = 1
+                },
+                new Tarea {
+                    Descripcion = "Crear Dto",
+                    FechaCreacion = DateTime.Now,
+                    IsTerminada = false,
+                    UsuarioId = 1
+                },
+                new Tarea {
+                    Descripcion = "Crear Servicios",
+                    FechaCreacion = DateTime.Now,
+                    IsTerminada = false,
+                    UsuarioId = 1
+                },
+                new Tarea {
+                    Descripcion = "Subir cambios al repo :v",
+                    FechaCreacion = DateTime.Now,
+                    IsTerminada = false,
+                    UsuarioId = 2
+                },
+                new Tarea {
+                    Descripcion = "Nota mental.. el niño sabe demasiado... :v",
+                    FechaCreacion = DateTime.Now,
+                    IsTerminada = false,
+                    UsuarioId = 2
+                },
+                new Tarea {
+                    Descripcion = "Crear Controllers",
+                    FechaCreacion = DateTime.Now,
+                    IsTerminada = false,
+                    UsuarioId = 1
+                }
+            };
+            context.Tarea.AddRange(tareas);
+            context.SaveChanges();                       
+        }
+    }
+}
